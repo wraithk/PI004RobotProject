13b38004f0f9ec2130bd78d51459803bcfc1bc52
Kirby Wraith <kirbywraith@Kirbys-MacBook-Pro.local>
2025-07-30 10:42:02 +1000

    lab finished

--- drive_circle.m
+++ drive_circle.m
@@ -1,8 +1,34 @@
 function drive_circle(pb, radius)
 % DRIVE_CIRCLE send a sequence of commands to the pibot to make it drive in a circle.
-
 % pb is the pibot instance to send commands
 % radius is the radius of the circle to drive
 
+velocity = 0.2;
+circumference = 2*pi*radius;
+driving_time = circumference/velocity;
+angular_velocity = (2*pi)/driving_time;
+[wl, wr] = inverse_kinematics(velocity, angular_velocity);
+
+% Maximum time per command
+max_time_per_command = 19;
+
+if driving_time <= max_time_per_command
+    % Single command if total time is 20 seconds or less
+    pb.setVelocity([wl wr], driving_time);
+else
+    % Break into multiple commands of 20 seconds each
+    num_full_segments = floor(driving_time / max_time_per_command);
+    remaining_time = driving_time - (num_full_segments * max_time_per_command);
+    
+    % Send full 20-second segments
+    for i = 1:num_full_segments
+        pb.setVelocity([wl wr], max_time_per_command);
+    end
+    
+    % Send remaining time segment if there's any left
+    if remaining_time > 0
+        pb.setVelocity([wl wr], remaining_time);
+    end
+end
 
 end
--- drive_square.m
+++ drive_square.m
@@ -1,8 +1,27 @@
 function drive_square(pb, sideLength)
 % DRIVE_SQUARE send a sequence of commands to the pibot to make it drive in a square.
-
 % pb is the pibot instance to send commands
 % sideLength is the length of each side of the square to drive
 
+drive_velocity = 0.2;
+ang_velocity = 1;
+
+side_length_time = sideLength/drive_velocity;
+corner_time = (pi/2)/ang_velocity;
+
+% Calculate wheel velocities for straight movement and rotation
+[wls, wrs] = inverse_kinematics(drive_velocity, 0);
+[wlc, wrc] = inverse_kinematics(0, ang_velocity);
+
+% Drive 5 squares
+for square = 1:5
+    % Drive each side of the square
+    for side = 1:4
+        % Drive straight for one side
+        pb.setVelocity([wls wrs], side_length_time);
+        % Rotate at corner (90 degrees)
+        pb.setVelocity([wlc wrc], corner_time);
+    end
+end
 
 end
