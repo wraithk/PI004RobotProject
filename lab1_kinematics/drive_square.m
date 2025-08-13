da8aaeecab91e46e2ba9260862942fadecee93a8
Sam Seabrook <102491464+SPROOK@users.noreply.github.com>
2025-08-13 10:28:31 +1000

    Lab1 W4: Submission

--- lab1_kinematics/drive_square.m
+++ lab1_kinematics/drive_square.m
@@ -8,78 +8,38 @@ ang_velocity = 2;
 
 side_length_time = sideLength/drive_velocity;
 corner_time = (pi/2)/ang_velocity;
-
+max_time_per_command = 4;
 
 % Calculate wheel velocities for straight movement and rotation
 [wls, wrs] = inverse_kinematics(drive_velocity, 0);
 [wlc, wrc] = inverse_kinematics(0, ang_velocity);
 
-% Drive 5 squares
 % Drive each side of the square
 for side = 1:4
-    % Drive straight for one side
-    disp(side_length_time);
-    pb.setVelocity([wls wrs], side_length_time/4);
-    pb.setVelocity([wls wrs], side_length_time/4);
-    pb.setVelocity([wls wrs], side_length_time/4);
-    pb.setVelocity([wls wrs], side_length_time/4);
-    % Rotate at corner (90 degrees)
-    pb.setVelocity([wlc wrc], corner_time);
-end
-end
-
-
-% Auto divides into a number of defined time segments
-
-
-%{
-function drive_square(pb, sideLength)
-% DRIVE_SQUARE send a sequence of commands to the pibot to make it drive in a square.
-% pb is the pibot instance to send commands
-% sideLength is the length of each side of the square to drive
-
-drive_velocity = 0.25;
-ang_velocity = 1.5;
-max_time_per_command = 4;
-
-% total times for one side and one corner
-side_time = sideLength/drive_velocity;
-turn_time = (pi/2)/ang_velocity;
-
-% wheel speeds for straight / rotation
-[wls, wrs] = inverse_kinematics(drive_velocity, 0);
-[wlc, wrc] = inverse_kinematics(0, ang_velocity);
-
-for side = 1:4
-    driving_time = side_time;
-    if driving_time <= max_time_per_command
-        pb.setVelocity([wls wrs], driving_time);
+    if side_length_time <= max_time_per_command
+        pb.setVelocity([wls wrs], side_length_time);
     else
-        num_full_segments = floor(driving_time / max_time_per_command);
-        remaining_time  = driving_time - num_full_segments*max_time_per_command;
+        num_full_segments = floor(side_length_time / max_time_per_command);
+        remaining_time = side_length_time - (num_full_segments * max_time_per_command);
+        
+        % Send full 4-second segments
         for i = 1:num_full_segments
             pb.setVelocity([wls wrs], max_time_per_command);
         end
-        if remaining_time > 0
-            pb.setVelocity([wls wrs], remaining_time);
-        end
-    end
 
-    % Rotate 90 at the corner 
-    rotating_time = turn_time;
-    if rotating_time <= max_time_per_command
-        pb.setVelocity([wlc wrc], rotating_time);
-    else
-        num_full_segments = floor(rotating_time / max_time_per_command);
-        remaining_time  = rotating_time - num_full_segments*max_time_per_command;
-        for i = 1:n_full
-            pb.setVelocity([wlc wrc], max_time_per_command);
-        end
         if remaining_time > 0
-            pb.setVelocity([wlc wrc], remaining_time);
+        pb.setVelocity([wls wrs], remaining_time);
         end
+
     end
+    % Send remaining time segment if there's any left
+   
+    % Rotate at corner (90 degrees)
+    pb.setVelocity([wlc wrc], corner_time);
+    disp('here')
 end
 end
 
-%}
+
+
+
--- lab1_kinematics/main_circle.m
+++ lab1_kinematics/main_circle.m
@@ -10,10 +10,10 @@ pb = PiBot('192.168.50.1');
 %pb.place([2.5;1.5], 0);
 
 % Drive a circle of radius 1m
-drive_circle(pb, 0.2);
+%drive_circle(pb, 1);
 
 % Place the robot at the bottom of another circle
-pb.place([2.5;0.5], 0);
+%pb.place([2.5;0.5], 0);
 
 % Drive a circle of radius 2m
-drive_circle(pb, 2);
+drive_circle(pb, 0.5);
